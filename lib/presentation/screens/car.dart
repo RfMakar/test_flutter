73f2187c4e1fcc4dd366100e5553877285e1a31b
Maxim <rfmakar@gmail.com>
2024-12-09 21:04:52 +0300

    app_bar

--- lib/presentation/screens/car.dart
+++ lib/presentation/screens/car.dart
@@ -10,10 +10,30 @@ class CarScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final car = ModalRoute.of(context)!.settings.arguments as Car;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(car.brand),
+      backgroundColor: const Color.fromRGBO(36, 44, 59, 1),
+      // appBar: AppBar(
+      //   title: Text(car.brand),
+      //   backgroundColor: Colors.white.withOpacity(0),
+      // ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset('assets/bg1.png'),
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 70, 0, 0),
+                height: 70,
+                color: Colors.amber,
+              ),
+              BodyCar(car: car),
+            ],
+          ),
+        ],
       ),
-      body: BodyCar(car: car),
     );
   }
 }
@@ -23,7 +43,8 @@ class BodyCar extends StatelessWidget {
   final Car car;
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
+      alignment: Alignment.bottomRight,
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(16),
@@ -49,29 +70,33 @@ class _LikeState extends State<Like> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        StatefulBuilder(
-          builder: (context, setState) {
-            return Row(
-              children: [
-                Text(
-                  likeCount.toString(),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() => likeCount++);
-                  },
-                  icon: likeCount == 0
-                      ? const Icon(Icons.favorite_outline)
-                      : const Icon(Icons.favorite),
-                )
-              ],
-            );
-          },
-        ),
-      ],
+    return StatefulBuilder(
+      builder: (context, setState) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              likeCount.toString(),
+              style: const TextStyle(color: Colors.red),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() => likeCount++);
+              },
+              icon: likeCount == 0
+                  ? const Icon(
+                      Icons.favorite_outline,
+                      color: Colors.red,
+                    )
+                  : const Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ),
+            )
+          ],
+        );
+      },
     );
   }
 }
